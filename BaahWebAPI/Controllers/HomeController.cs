d25562e52385243a0eb7822ba941afa434b28d22
Theophil Ronald <ronaldtdas@gmail.com>
2023-05-22 00:27:18 +0600

    update

--- BaahWebAPI/Controllers/HomeController.cs
+++ BaahWebAPI/Controllers/HomeController.cs
@@ -11,6 +11,7 @@ namespace BaahWebAPI.Controllers
     {
         private readonly ILogger<modelHome> _logger;
         clsDapper dapper = new clsDapper();
+        clsUtility utility = new clsUtility();
         //----Demo----//
         //var aaa = dapper.Con().Query<ViewSalesreport>("select * from view_salesreport");
 
@@ -28,21 +29,28 @@ namespace BaahWebAPI.Controllers
 
 
             modelHome model = new modelHome();
-            //string query = "SELECT DATE(`wp_c84s672ma8_wc_order_stats`.`date_created`) AS `Date`, SUM(`wp_c84s672ma8_wc_order_stats`.`num_items_sold`) AS `ItemsSold`, SUM(`wp_c84s672ma8_wc_order_stats`.`total_sales`) AS `TotalSale`, (((SELECT meta_value FROM baahstore.wp_c84s672ma8_postmeta WHERE post_id =`wp_c84s672ma8_wc_order_stats`.`order_id` AND meta_key = '_alg_wc_cog_order_profit') + (SELECT meta_value FROM baahstore.wp_c84s672ma8_postmeta WHERE post_id =`wp_c84s672ma8_wc_order_stats`.`order_id` AND meta_key = '_order_shipping')) - (SELECT meta_value FROM baahstore.wp_c84s672ma8_postmeta WHERE post_id =`wp_c84s672ma8_wc_order_stats`.`order_id` AND meta_key = '_wc_cost_of_shipping')) AS `TotalProfit` FROM `wp_c84s672ma8_wc_order_stats` WHERE CAST(`wp_c84s672ma8_wc_order_stats`.`date_created` AS DATE) BETWEEN CAST('" + fDate + "' AS DATE) AND CAST('" + tDate + "' AS DATE) GROUP BY DATE(`wp_c84s672ma8_wc_order_stats`.`date_created`) ORDER BY `Date` DESC";
-            //var list = dapper.Con().Query<Profit>(query).ToList();
+
+            string fDateM = utility.GetDateString(1, "dd MMM, yyyy");
+            string tDateM = utility.GetDateString(2, "dd MMM, yyyy");
+
+            string query1 = "SELECT SUM(TotalSale) FROM view_salesreport WHERE CAST(DATE AS DATE) Between Cast('" + fDateM + "' as Date) and Cast('" + tDateM + "' as Date)";
+            model.LastMonthSale = dapper.Con().Query<decimal>(query1).FirstOrDefault();
+
+            //string query2 = "SELECT SUM(TotalSale) FROM view_salesreport WHERE CAST(DATE AS DATE) Between Cast('" + fDate + "' as Date) and Cast('" + tDate + "' as Date)";
+            //model.LastMonthSale = dapper.Con().Query<decimal>(query2).FirstOrDefault();
 
 
 
 
-            string query2 = "select DATE_FORMAT(cast(Date as date),'%d/%m/%Y') as Date,sum(TotalSale) as TotalSale from view_salesreport where cast(Date as Date) Between Cast('" + fDate + "' as Date) and Cast('" + tDate + "' as Date) group by CAST(Date as Date)";
-            var sales = dapper.Con().Query<Sale>(query2).ToList();
+            string query6 = "select DATE_FORMAT(cast(Date as date),'%d/%m/%Y') as Date,sum(TotalSale) as TotalSale from view_salesreport where cast(Date as Date) Between Cast('" + fDate + "' as Date) and Cast('" + tDate + "' as Date) group by CAST(Date as Date)";
+            var sales = dapper.Con().Query<Sale>(query6).ToList();
             //List<Sale> sales = new List<Sale>();
             model.Trend = sales;
 
 
 
-            string query3 = "select CategoryId, CategoryName, SUM(ProductSoldQty) as ItemsSold , SUM(ProductSoldAmount) as TotalAmount from View_OrderDetail where cast(Date as Date) Between Cast('" + fDate + "' as Date) and Cast('" + tDate + "' as Date) group by CategoryId, CategoryName order by ItemsSold desc";
-            var catlist = dapper.Con().Query<CategorywiseSale>(query3).ToList();
+            string query7 = "select CategoryId, CategoryName, SUM(ProductSoldQty) as ItemsSold , SUM(ProductSoldAmount) as TotalAmount from View_OrderDetail where cast(Date as Date) Between Cast('" + fDate + "' as Date) and Cast('" + tDate + "' as Date) group by CategoryId, CategoryName order by ItemsSold desc";
+            var catlist = dapper.Con().Query<CategorywiseSale>(query7).ToList();
             model.CategorywiseSales = catlist;
 
             return model;
---
+++ BaahWebAPI/clsUtility.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace BaahWebAPI
+{
+    public class clsUtility
+    {
+
+        public string GetDateString(int type, string format)
+        {
+            DateTime givenDate = DateTime.Now;
+            DateTime result = DateTime.Now;
+            if (type == 1)
+            {
+                result = new DateTime(givenDate.Year, givenDate.Month, 1);
+            }
+            else if (type == 2)
+            {
+                // Get the first day of the next month
+                DateTime nextMonth = givenDate.AddMonths(1);
+                DateTime firstDayOfNextMonth = new DateTime(nextMonth.Year, nextMonth.Month, 1);
+
+                // Subtract one day to get the last day of the current month
+                result = firstDayOfNextMonth.AddDays(-1);
+            }
+
+            return result.ToString(format);
+        }
+    }
+}
