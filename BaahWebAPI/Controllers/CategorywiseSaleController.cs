a12427928ff719462d28b4919f58d69d447c5db8
Theophil Ronald <ronaldtdas@gmail.com>
2023-05-31 18:41:41 +0600

    Update CategorywiseSaleController.cs

--- BaahWebAPI/Controllers/CategorywiseSaleController.cs
+++ BaahWebAPI/Controllers/CategorywiseSaleController.cs
@@ -50,41 +50,48 @@ namespace BaahWebAPI.Controllers
 
             string query = "select CategoryId, CategoryName, SUM(ProductSoldQty) as ItemsSold , SUM(ProductSoldAmount) as TotalAmount from View_OrderDetail where cast(Date as Date) Between Cast('" + fDate + "' as Date) and Cast('" + tDate + "' as Date) and categoryid = '" + id + "' group by CategoryId, CategoryName order by ItemsSold desc";
             var item = dapper.Con().Query<CategorywisePerfomance>(query).FirstOrDefault();
-            item.AverageOrderValue = (Convert.ToDecimal(item.TotalAmount) / Convert.ToInt32(item.ItemsSold)).ToString();
-
-            DateTime from = DateTime.ParseExact(fDate, "yyyy-MM-dd", System.Globalization.CultureInfo.InvariantCulture);
-            DateTime to = DateTime.ParseExact(tDate, "yyyy-MM-dd", System.Globalization.CultureInfo.InvariantCulture);
-            TimeSpan timeDifference = to - from;
-            double daysDifference = timeDifference.TotalDays;
-
-            string query2 = "";
-            if (daysDifference < 32)
+            if (item != null)
             {
+                item.AverageOrderValue = (Convert.ToDecimal(item.TotalAmount) / Convert.ToInt32(item.ItemsSold)).ToString();
+
+                DateTime from = DateTime.ParseExact(fDate, "yyyy-MM-dd", System.Globalization.CultureInfo.InvariantCulture);
+                DateTime to = DateTime.ParseExact(tDate, "yyyy-MM-dd", System.Globalization.CultureInfo.InvariantCulture);
+                TimeSpan timeDifference = to - from;
+                double daysDifference = timeDifference.TotalDays;
+
+                string query2 = "";
+                if (daysDifference < 32)
+                {
+                    //WHERE View_OrderDetail.Status = 'wc-completed' AND
+                    query2 = "SELECT CAST(DATE AS DATE) AS DateString, SUM(ProductSoldAmount) AS TotalSale FROM View_OrderDetail WHERE cast(Date as Date) Between Cast('" + fDate + "' as Date) and Cast('" + tDate + "' as Date) AND CategoryId = '" + id + "' GROUP BY DateString";
+                }
+                else if (daysDifference > 31 && daysDifference < 366)
+                {
+                    //WHERE View_OrderDetail.Status = 'wc-completed' AND
+                    query2 = "SELECT CONCAT(SUBSTRING(MONTHNAME(`DATE`), 1, 3), '-', RIGHT(YEAR(`DATE`), 2)) AS DateString, SUM(ProductSoldAmount) AS TotalSale FROM View_OrderDetail  WHERE cast(Date as Date) Between Cast('" + fDate + "' as Date) and Cast('" + tDate + "' as Date) AND CategoryId = '" + id + "' GROUP BY DateString ORDER BY YEAR(`DATE`), MONTH(`DATE`)";
+                }
+                else if (daysDifference > 365)
+                {
+                    //WHERE View_OrderDetail.Status = 'wc-completed' AND
+                    query2 = "SELECT YEAR(`DATE`) AS DateString, SUM(ProductSoldAmount) AS TotalSale FROM View_OrderDetail  WHERE cast(Date as Date) Between Cast('" + fDate + "' as Date) and Cast('" + tDate + "' as Date) AND CategoryId = '" + id + "' GROUP BY DateString";
+                }
+
+
+                var datewiseSales = dapper.Con().Query<DatewiseSales>(query2).ToList();
+                item.DatewiseSales = datewiseSales;
+
+
                 //WHERE View_OrderDetail.Status = 'wc-completed' AND
-                query2 = "SELECT CAST(DATE AS DATE) AS DateString, SUM(ProductSoldAmount) AS TotalSale FROM View_OrderDetail WHERE cast(Date as Date) Between Cast('" + fDate + "' as Date) and Cast('" + tDate + "' as Date) AND CategoryId = '" + id + "' GROUP BY DateString";
+                string query3 = "SELECT ProductName, ProductSoldAmount AS totalSale FROM View_OrderDetail  WHERE cast(Date as Date) Between Cast('" + fDate + "' as Date) and Cast('" + tDate + "' as Date) AND CategoryId = '" + id + "' ORDER BY ProductSoldAmount DESC LIMIT 5";
+                var list = dapper.Con().Query<TopSellingProduct>(query3).ToList();
+                item.TopSellingProducts = list;
+                return item;
             }
-            else if(daysDifference > 31 && daysDifference < 366)
+            else
             {
-                //WHERE View_OrderDetail.Status = 'wc-completed' AND
-                query2 = "SELECT CONCAT(SUBSTRING(MONTHNAME(`DATE`), 1, 3), '-', RIGHT(YEAR(`DATE`), 2)) AS DateString, SUM(ProductSoldAmount) AS TotalSale FROM View_OrderDetail  WHERE cast(Date as Date) Between Cast('" + fDate + "' as Date) and Cast('" + tDate + "' as Date) AND CategoryId = '" + id + "' GROUP BY DateString ORDER BY YEAR(`DATE`), MONTH(`DATE`)";
-            }
-            else if(daysDifference > 365)
-            {
-                //WHERE View_OrderDetail.Status = 'wc-completed' AND
-                query2 = "SELECT YEAR(`DATE`) AS DateString, SUM(ProductSoldAmount) AS TotalSale FROM View_OrderDetail  WHERE cast(Date as Date) Between Cast('" + fDate + "' as Date) and Cast('" + tDate + "' as Date) AND CategoryId = '" + id + "' GROUP BY DateString";
+                return new CategorywisePerfomance();
             }
 
-            
-            var datewiseSales = dapper.Con().Query<DatewiseSales>(query2).ToList();
-            item.DatewiseSales = datewiseSales;
-
-
-            //WHERE View_OrderDetail.Status = 'wc-completed' AND
-            string query3 = "SELECT ProductName, ProductSoldAmount AS totalSale FROM View_OrderDetail  WHERE cast(Date as Date) Between Cast('" + fDate + "' as Date) and Cast('" + tDate + "' as Date) AND CategoryId = '" + id + "' ORDER BY ProductSoldAmount DESC LIMIT 5";
-            var list = dapper.Con().Query<TopSellingProduct>(query3).ToList();
-            item.TopSellingProducts = list;
-
-            return item;
         }
     }
 }
