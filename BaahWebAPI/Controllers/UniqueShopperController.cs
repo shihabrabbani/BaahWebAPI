10772b74b1e7098bb9cb96f091d2508ffdf74c31
Theophil Ronald <ronaldtdas@gmail.com>
2023-07-05 23:02:27 +0600

    update

---
+++ BaahWebAPI/Controllers/UniqueShopperController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Dapper;
+using BaahWebAPI.DapperModels;
+
+namespace BaahWebAPI.Controllers
+{
+    [ApiController]
+    public class UniqueShopperController : ControllerBase
+    {
+        private readonly ILogger<UniqueShopperController> _logger;
+        clsDapper dapper = new clsDapper();
+
+        public UniqueShopperController(ILogger<UniqueShopperController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("UniqueShopper")]
+        public IEnumerable<view_shoppers> Get()
+        {
+            string fDate = DateTime.Now.AddDays(-7).ToString("yyyy-MM-dd");
+            string tDate = DateTime.Now.ToString("yyyy-MM-dd");
+
+            string query = "SELECT * FROM baahstore.view_shoppers WHERE CAST(DateString AS DATE) BETWEEN  Cast('" + fDate + "' as Date) and Cast('" + tDate + "' as Date) ";
+            var list = dapper.Con().Query<view_shoppers>(query).OrderBy(f=>f.Id).ToList();
+
+            return list;
+        }
+
+
+        [HttpGet(("UniqueShopper/{FromDate}&{ToDate}"))]
+        public IEnumerable<view_shoppers> Get(string FromDate, string ToDate)
+        {
+            string fDate = FromDate;
+            string tDate = ToDate;
+
+            string query = "SELECT * FROM baahstore.view_shoppers WHERE CAST(DateString AS DATE) BETWEEN  Cast('" + fDate + "' as Date) and Cast('" + tDate + "' as Date) ";
+            var list = dapper.Con().Query<view_shoppers>(query).OrderBy(f => f.Id).ToList();
+
+            return list;
+        }
+    }
+}
---
+++ BaahWebAPI/DapperModels/view_shoppers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BaahWebAPI.DapperModels
+{
+    public class view_shoppers
+    { 
+        public int Id { get; set; }
+        public int OrderNumber { get; set; }
+        public string DateString { get; set; }
+        public string Status { get; set; }
+        public double OrderAmount { get; set; }
+        public string PhoneNumber { get; set; }
+        public int ReturnCount { get; set; }
+    }
+}
